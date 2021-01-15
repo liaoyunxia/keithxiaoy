0aae64b19999f4e427d3d768f44e403b8bc8fcd9
“liaoliao <372021030@qq.com>
2021-01-15 17:41:21 +0800

    Update database_router.py

--- keithxiaoy/database_router.py
+++ keithxiaoy/database_router.py
@@ -1,16 +1,72 @@
 import random
 
 
-class MasterSlaveRouter(object):
+class ClientRouter:
+    """
+    A router to control all database operations on models in the
+    client application.
+    """
 
     def db_for_read(self, model, **hints):
-        return random.choice(['default'])
+        """
+        Attempts to read client models go to client.
+        """
+        if model._meta.app_label == 'client':
+            return 'client'
+        return None
 
     def db_for_write(self, model, **hints):
-        return 'default'
+        """
+        Attempts to write client models go to client.
+        """
+        if model._meta.app_label == 'client':
+            return 'client'
+        return None
 
     def allow_relation(self, obj1, obj2, **hints):
-        return True
+        """
+        Allow relations if a model in the client app is involved.
+        """
+        if obj1._meta.app_label == 'client' or \
+                obj2._meta.app_label == 'client':
+            return True
+        return None
 
-    def allow_syncdb(self, db, model):
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """
+        Make sure the client app only appears in the 'client'
+        database.
+        """
+        if app_label == 'client':
+            return db == 'client'
+        return None
+
+
+class PrimaryReplicaRouter:
+    def db_for_read(self, model, **hints):
+        """
+        Reads go to a randomly-chosen replica.
+        """
+        return random.choice(['primary'])
+
+    def db_for_write(self, model, **hints):
+        """
+        Writes always go to primary.
+        """
+        return 'primary'
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Relations between objects are allowed if both objects are
+        in the primary/replica pool.
+        """
+        db_list = ('primary',)
+        if obj1._state.db in db_list and obj2._state.db in db_list:
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """
+        All non-auth models end up in this pool.
+        """
         return True
