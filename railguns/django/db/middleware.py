0a3ae568352bea4cf84bf2452e334ebff258d9f3
“liaoliao <372021030@qq.com>
2021-01-16 03:08:46 +0800

    Update middleware.py

--- railguns/django/db/middleware.py
+++ railguns/django/db/middleware.py
@@ -8,6 +8,14 @@ class MultiDBRouterMiddleware(object):
     https://docs.djangoproject.com/en/dev/topics/http/middleware/#process-view
     https://github.com/transientskp/banana/blob/master/project/multidb.py
     """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
     def process_view(self, request, view_func, view_args, view_kwargs):
         request_cfg.pk = view_kwargs.get('pk')
 
