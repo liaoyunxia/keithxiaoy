60697ddb3872b805847f190985a1b210cb242c20
“liaoliao <372021030@qq.com>
2021-01-21 02:23:46 +0800

    Update urls.py

--- keithxiaoy/urls.py
+++ keithxiaoy/urls.py
@@ -9,18 +9,18 @@ urlpatterns = [path('', include('railguns.urls'))]
 urlpatterns += [path('s3direct/', include('s3direct.urls'))]
 # projects
 urlpatterns += [
-    path('', include(('{}.apps.home.urls'.format(settings.PROJECT_NAME), 'v1'), namespace='website')),
-    path('api/v1/', include(('{}.apps.api.urls'.format(settings.PROJECT_NAME), 'v1'), namespace='v1')),
-    # path('api/v1/agreement/', include('{}.apps.agreements.urls'.format(settings.PROJECT_NAME), namespace='v1')),
-    path('i18n/', include('django.conf.urls.i18n')),
-    path(
-        'robots.txt',
-        cache_page(60 * 60)(
-            TemplateView.as_view(
-                template_name='robots_{}.txt'.format(settings.ENV.name.lower()), content_type='text/plain'
-            )
-        )
-    )
+    path('', include(('{}.apps.home.urls'.format(settings.PROJECT_NAME), 'home'), namespace='v1')),
+    path('api/v1/', include(('{}.apps.api.urls'.format(settings.PROJECT_NAME), 'api'), namespace='v1')),
+    # # path('api/v1/agreement/', include('{}.apps.agreements.urls'.format(settings.PROJECT_NAME), namespace='v1')),
+    path('i18n/', include(('django.conf.urls.i18n', 'i18n'), 'i18n')),
+    # path(
+    #     'robots.txt',
+    #     cache_page(60 * 60)(
+    #         TemplateView.as_view(
+    #             template_name='robots_{}.txt'.format(settings.ENV.name.lower()), content_type='text/plain'
+    #         )
+    #     )
+    # )
 ]
 
 
