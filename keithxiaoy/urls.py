2acc3cf5f9be57efde16c6781e5d23f1f0f0a157
“liaoliao <372021030@qq.com>
2021-01-22 00:32:37 +0800

    fix bug

--- keithxiaoy/urls.py
+++ keithxiaoy/urls.py
@@ -49,7 +49,7 @@ urlpatterns += [
 urlpatterns += [
     # url('', include(('{}.apps.urls'.format(settings.PROJECT_NAME), 'apps'), namespace='apps-urls')),
     url(r'^api/v1/', include(('{}.apps.api.urls'.format(settings.PROJECT_NAME), 'v1'), namespace='v1')),
-    url(r'home', include('{}.apps.home.urls'.format(settings.PROJECT_NAME, 'home'))),
+    url(r'', include('{}.apps.home.urls'.format(settings.PROJECT_NAME, 'home'))),
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
