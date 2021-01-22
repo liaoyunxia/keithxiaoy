14e8809d9ccc50add5ca8a4af162ce5cc82273db
“liaoliao <372021030@qq.com>
2021-01-22 16:04:34 +0800

    Update admin.py

--- keithxiaoy/apps/articles/admin.py
+++ keithxiaoy/apps/articles/admin.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from django.contrib import admin, auth
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserChangeForm
 from django.utils.translation import ugettext_lazy as _
 # from import_export.admin import ImportExportMixin
 
@@ -32,7 +31,6 @@ class ArticleAdmin(admin.ModelAdmin):
     list_filter = ['import_method', 'user', 'state', 'type']
     suit_list_filter_horizontal = ['import_method', 'name', 'state']
     search_fields = ['name', 'user__username']
-    # filter_horizontal = ('groups', 'user_permissions', )
 
 
 admin.site.register(Article, ArticleAdmin)
