a2d97d0e67ac5ee647f4aeed0d85e0bc359f003a
“liaoliao <372021030@qq.com>
2021-01-27 14:56:00 +0800

    Update admin.py

--- keithxiaoy/apps/articles/admin.py
+++ keithxiaoy/apps/articles/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils.html import format_html
 from django.contrib import admin
 from django.contrib import admin, auth
 from django.contrib.auth import get_user_model
@@ -15,22 +15,26 @@ class ArticleAdmin(admin.ModelAdmin):
     image_height = 32
 
     fieldsets = (
-        (None, {'fields': ('name', 'no', 'english_name', 'bg_img', 'preview','priority', 'category', 'set_top')}),
+        (None, {'fields': ('name', 'no', 'english_name', 'bg_img','priority', 'category', 'set_top')}),
         (_('article content'), {'fields': ('remark', 'url', 'summary', 'content')}),
         (_('article state'), {'fields': ('type', 'state', 'import_method', 'user')}),
         # (_('time'), {'fields': ('modify_time',)}),
     )
 
     add_fieldsets = (
-        (None, {'fields': ('name', 'no', 'english_name', 'bg_img', 'preview', 'priority', 'category', 'set_top')}),
+        (None, {'fields': ('name', 'no', 'english_name', 'bg_img', 'priority', 'category', 'set_top')}),
         (_('article content'), {'fields': ('remark', 'url', 'summary', 'content', )}),
         (_('article state'), {'fields': ('type', 'state', 'import_method', 'user')}),
     )
 
-    list_display = ['id', 'priority', 'bg_img', 'name', 'english_name', 'category', 'type', 'import_method', 'url', 'remark','user', 'state','modify_time']
+    list_display = ['id', 'priority', 'get_preview', 'name', 'english_name', 'category', 'type', 'import_method', 'url', 'remark','user', 'state','modify_time']
     list_filter = ['import_method', 'user', 'state', 'type', 'category', ]
     suit_list_filter_horizontal = ['import_method', 'name', 'state']
     search_fields = ['name', 'user__username', 'url', 'english_name']
+
+    def get_preview(self, obj):
+        return format_html('<a href="{0}" rel="external"><img src="/media{0}" width="50" height="50"/></a>'.format(obj.bg_img))
+    get_preview.short_description = _('preview')
 
 
 admin.site.register(Article, ArticleAdmin)
